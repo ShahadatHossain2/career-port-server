0a8440b555d4f78bd047b9e4ad745ca2fb9e4cdb
ShahadatHossain2 <mdshahadat6868@gmail.com>
2026-01-22 18:44:51 +0600

    Firebase admin using env variables

--- index.js
+++ index.js
@@ -8,12 +8,21 @@ const cookieParser = require('cookie-parser');
 
 const port = process.env.PORT || 5000;
 
-var admin = require("firebase-admin");
+import admin from "firebase-admin";
+// var admin = require("firebase-admin");
 
-var serviceAccount = require("./firebase-admin-key.json");
+// var serviceAccount = require("./firebase-admin-key.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
 
 
